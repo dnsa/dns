519362119df3f6e53354fc1af93bd61542e0cdb4
dnsa <61642888+dnsa@users.noreply.github.com>
2020-05-28 09:22:07 +0700

    Update gravity.db.sql

--- gravity.db.sql
+++ gravity.db.sql
@@ -1,5 +1,5 @@
 insert into"group"(id,name)values(1,'dnsb');
-insert into adlist(id,address)values(0,'https://raw.githubusercontent.com/ookangzheng/dbl-oisd-nl/master/dbl.txt');
+insert into adlist(id,address)values(0,'https://hosts.oisd.nl');
 insert into adlist_by_group(adlist_id,group_id)values(0,1);
 insert into client(id,ip)values(0,'xxx');
 delete from client_by_group;
